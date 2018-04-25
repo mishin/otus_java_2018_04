ba88a8bd6acd2934dd2d1dcc1c7d2799fb118223
Dmitry Shirokov <Dmitry_Shirokov@epam.com>
2018-04-25 19:15:52 +0300

    added arguments in main class

--- L01.1-maven/src/main/java/ru/otus/l011/Main.java
+++ L01.1-maven/src/main/java/ru/otus/l011/Main.java
@@ -1,22 +1,28 @@
 package ru.otus.l011;
 
 import java.util.*;
-
-import com.github.javafaker.Faker; //client's generator
-import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
+import com.github.javafaker.Faker; //client's generator
 
-import javax.swing.*;
+
+/**
+ * Created by Dmitry.
+ *
+ * Example for L01.1
+ *
+ */
 
 public class Main {
 
-    private static final int CLIENT_COUNT = 100;
-    private static final int MEASURE_COUNT = 10;
+    private static boolean debugMode = false;
+    private static int clientCount = 20;
+    private static int measureCount = 10;
 
-    private static void printFaker(Faker faker){
+    private static void printClient(Faker faker){
         StringBuffer fakerStringBuf = new StringBuffer();
-        fakerStringBuf.append(faker.name().fullName())
+        fakerStringBuf.append("Profile: ")
+                .append(faker.name().fullName())
                 .append(" lives in ")
                 .append(faker.address().fullAddress());
         System.out.println(fakerStringBuf);
@@ -24,46 +30,68 @@ public class Main {
 
     public static void main(String... args){
 
-        //List<String> names = Arrays.asList("John", "Adam", "Jane","David","Mike","Dmitry","Samuel");
+
+        /* Arguments
+        1 arg - Debug mode (default is false)
+        2 arg - Number of client to be generated
+        3 arg - Number of iteration to estimate
+        */
+
+        if (args.length > 0) {
+            debugMode = args[0].equalsIgnoreCase("debug") ? true: false;
+            try {
+                // Parse the string argument into an integer value.
+                clientCount = Integer.parseInt(args[1]);
+                measureCount = Integer.parseInt(args[2]);
+            }
+            catch (NumberFormatException | ArrayIndexOutOfBoundsException exception) {
+                System.out.println("The 2nd and 3rd argument must be an integer.");
+                System.exit(1);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Program args: ")
+                .append(" debugMode: ").append(debugMode)
+                .append(" clientCount: ").append(clientCount)
+                .append(" measureCount: ").append(measureCount);
+        System.out.println(stringBuilder);
 
         //Generation list of client with address
         List<Faker> clients = new ArrayList<>();
         List<String> names  = new ArrayList<>();
-        //Collection<String> = new ImmutableSet<>();
 
-        for (int i = 0; i<CLIENT_COUNT; i++) {
+        for (int i = 0; i<clientCount; i++) {
             Faker client = new Faker();
             clients.add(client);
             names.add(clients.get(i).name().firstName());
-           // printFaker(client);
+            if (debugMode) { printClient(client);}
         }
 
-        for (int i = 0; i<CLIENT_COUNT; i++) {
-            Faker client = clients.get(i);
-            //printFaker(client);
-            //System.out.println(client.name().firstName());
-        }
+        StringBuffer sb = new StringBuffer();
+        String str = String.join(", ", names);
+        sb.append("All of names: ")
+                .append(str);
+        System.out.print(sb);
 
-        //collect only unique client's name
-        //ImmutableCollection<String> names_unique = ImmutableSet.copyOf(names).asList();
+        System.out.print("Operations: ");
+        //estimate the operation called collect only unique client's name
         calcTime(() -> ImmutableSet.copyOf(names).asList() ,"Uniqueness");
 
-        List<String> reversed_names = new ArrayList<>();
-
-
+        //estimate the operation called reverse name
         List<String> reverse_names = new ArrayList<>();
         Collections.shuffle((List<String>)names);
         calcTime(() -> reverse_names.addAll(Lists.reverse((List<String>)names)), "Reverse of list");
-
 
     }
 
     private static void calcTime(Runnable runnable, String... addArgs) {
         long startTime = System.nanoTime();
-        for (int i = 0; i < MEASURE_COUNT; i++)
+        for (int i = 0; i < measureCount; i++)
             runnable.run();
         long finishTime = System.nanoTime();
-        long timeNs = (finishTime - startTime) / MEASURE_COUNT;
+        long timeNs = (finishTime - startTime) / measureCount;
+
+        System.out.println();
         System.out.println("Operation: " + addArgs[0]);
         System.out.println("Time spent: " + timeNs + "ns (" + timeNs / 1_000_000 + "ms)");
     }
--- README.md
+++ README.md
@@ -2,12 +2,32 @@
 Группа 2018-04
 
 
-### Преподаватели
-Vitaly Chibrikov (Виталий Чибриков)
+#### Преподаватели
+* Vitaly Chibrikov (Виталий Чибриков)
 chibrikov@otus.ru
 https://github.com/vitaly-chibrikov
 
-
-
-Vladimir Sonkin (Владимир Сонькин)
+* Vladimir Sonkin (Владимир Сонькин)
 vladson@ya.ru
+
+### Задания
+
+#### ТЗ №1
+
+ To start the application:
+ * cd L01.1-maven
+ * mvn package
+ * java -cp ./target/L01.1-maven.jar ru.otus.l011.Main
+ или java -jar ./target/L01.1-maven.jar 
+ 
+ 
+ * To unzip the jar:
+ * 7z x -oJAR ./target/L01.1-maven.jar
+ * unzip -d JAR ./target/L01.1-maven.jar
+ 
+
+ To build:
+ * mvn package
+ * mvn clean compile
+ * mvn assembly:single
+ * mvn clean compile assembly:single
